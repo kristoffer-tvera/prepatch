f7d985b585991d10f93c1fc920e1533a3a3d758c
Kristoffer Tverå <Bluekristoffer@gmail.com>
2020-11-12 19:59:40 +0100

    show server-time, and correct timer for rares

--- src/App.vue
+++ src/App.vue
@@ -31,19 +31,17 @@
     </div>
   </nav>
 
-  <h1 class="my-4 py-4">{{ currentTime }}</h1>
-  <div aria-live="polite" aria-atomic="true" style="position: fixed; min-height: 100vh; width: 100%;">
+  <h1 class="my-4 py-4">Server Time: {{ currentTime }}</h1>
+  <div aria-live="polite" aria-atomic="true" style="position: fixed; min-height: 100vh; width: 100%">
     <div
       class="toast d-flex align-items-center"
       id="clipboard"
       role="alert"
       aria-live="assertive"
       aria-atomic="true"
-      style="position: absolute; top: 0; right: 0;"
+      style="position: absolute; top: 0; right: 0"
     >
-      <div class="toast-body">
-        Copied waypoint to clipboard!
-      </div>
+      <div class="toast-body">Copied waypoint to clipboard!</div>
       <button type="button" class="btn-close ml-auto mr-2" data-dismiss="toast" aria-label="Close"></button>
     </div>
   </div>
@@ -70,17 +68,19 @@ export default {
   name: 'App',
   components: {},
   mounted() {
+    this.updateTime();
     setInterval(this.updateTime, 1000);
     this.updateRotation();
 
-    for (let i = 0; i < this.rotation.length; i++) {
-      this.rotation[i].nextSpawn = ((i * 20) % 60).toString();
-    }
+    // for (let i = 0; i < this.rotation.length; i++) {
+    //   this.rotation[i].nextSpawn = ((i * 20) % 60).toString();
+    // }
   },
 
   data() {
     return {
-      currentTime: '',
+      timeBetweenBossSpawns: 20,
+      currentTime: '__:__:__',
       links: [
         {
           id: 1,
@@ -105,43 +105,35 @@ export default {
         },
       ],
       rotation: [
-        { id: 0, name: 'The Black Knight', waypoint: '/way 64,8, 22,1', active: false, nextSpawn: '' },
-        { id: 1, name: 'Bronjahm', waypoint: '/way 70,7, 38,4', active: false, nextSpawn: '' },
-        { id: 2, name: 'Scourgelord Tyrannus', waypoint: '/way 47,2, 66,1', active: false, nextSpawn: '' },
-        { id: 3, name: 'Forgemaster Garfrost', waypoint: '/way 58,6, 72,5', active: false, nextSpawn: '' },
-        { id: 4, name: 'Marwyn', waypoint: '/way 58,2, 83,4', active: false, nextSpawn: '' },
-        { id: 5, name: 'Falric', waypoint: '/way 50,2, 87,9', active: false, nextSpawn: '' },
-        { id: 6, name: "The Prophet Tharon'ja", waypoint: '/way 80,1, 61,2', active: false, nextSpawn: '' },
-        { id: 7, name: 'Novos the Summoner', waypoint: '/way 77,8, 66,1', active: false, nextSpawn: '' },
-        { id: 8, name: 'Trollgore', waypoint: '/way 58,3, 39,4', active: false, nextSpawn: '' },
-        { id: 9, name: "Krik'thir the Gatewatcher", waypoint: '/way 67,5, 58,0', active: false, nextSpawn: '' },
-        { id: 10, name: 'Prince Taldaram', waypoint: '/way 29,6, 62,2', active: false, nextSpawn: '' },
-        { id: 11, name: 'Elder Nadox', waypoint: '/way 44,2, 49,1', active: false, nextSpawn: '' },
-        { id: 12, name: 'Noth the Plaguebringer', waypoint: '/way 31,6, 70,5', active: false, nextSpawn: '' },
-        { id: 13, name: 'Patchwerk', waypoint: '/way 36,5, 67,4', active: false, nextSpawn: '' },
-        { id: 14, name: "Blood Queen Lana'thel", waypoint: '/way 49,7, 32,7', active: false, nextSpawn: '' },
-        { id: 15, name: 'Professor Putricide', waypoint: '/way 57,1, 30,3', active: false, nextSpawn: '' },
-        { id: 16, name: 'Lady Deathwhisper', waypoint: '/way 51,1, 78,5', active: false, nextSpawn: '' },
-        { id: 17, name: 'Skadi the Ruthless', waypoint: '/way 57,8, 56,1', active: false, nextSpawn: '' },
-        { id: 18, name: 'Ingvar the Plunderer', waypoint: '/way 52,4, 52,6', active: false, nextSpawn: '' },
+        { id: 0, name: 'The Black Knight', waypoint: '/way 64,8, 22,1', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 1, name: 'Bronjahm', waypoint: '/way 70,7, 38,4', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 2, name: 'Scourgelord Tyrannus', waypoint: '/way 47,2, 66,1', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 3, name: 'Forgemaster Garfrost', waypoint: '/way 58,6, 72,5', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 4, name: 'Marwyn', waypoint: '/way 58,2, 83,4', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 5, name: 'Falric', waypoint: '/way 50,2, 87,9', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 6, name: "The Prophet Tharon'ja", waypoint: '/way 80,1, 61,2', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 7, name: 'Novos the Summoner', waypoint: '/way 77,8, 66,1', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 8, name: 'Trollgore', waypoint: '/way 58,3, 39,4', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 9, name: "Krik'thir the Gatewatcher", waypoint: '/way 67,5, 58,0', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 10, name: 'Prince Taldaram', waypoint: '/way 29,6, 62,2', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 11, name: 'Elder Nadox', waypoint: '/way 44,2, 49,1', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 12, name: 'Noth the Plaguebringer', waypoint: '/way 31,6, 70,5', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 13, name: 'Patchwerk', waypoint: '/way 36,5, 67,4', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 14, name: "Blood Queen Lana'thel", waypoint: '/way 49,7, 32,7', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 15, name: 'Professor Putricide', waypoint: '/way 57,1, 30,3', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 16, name: 'Lady Deathwhisper', waypoint: '/way 51,1, 78,5', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 17, name: 'Skadi the Ruthless', waypoint: '/way 57,8, 56,1', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 18, name: 'Ingvar the Plunderer', waypoint: '/way 52,4, 52,6', active: false, nextSpawn: '', sortOrder: -1 },
+        { id: 19, name: 'Prince Keleseth', waypoint: '/way 54,0, 44,7', active: false, nextSpawn: '', sortOrder: -1 },
       ],
     };
   },
   methods: {
     updateTime() {
       let date = new Date();
-      let hour = date
-        .getUTCHours()
-        .toString()
-        .padStart(2, '0');
-      let minute = date
-        .getUTCMinutes()
-        .toString()
-        .padStart(2, '0');
-      let seconds = date
-        .getUTCSeconds()
-        .toString()
-        .padStart(2, '0');
+      let hour = (date.getUTCHours() + (1 % 24)).toString().padStart(2, '0');
+      let minute = date.getUTCMinutes().toString().padStart(2, '0');
+      let seconds = date.getUTCSeconds().toString().padStart(2, '0');
 
       if (seconds === '00') {
         this.updateRotation();
@@ -151,14 +143,27 @@ export default {
     },
 
     updateRotation() {
-      let epoch = Date.UTC(2020, 10, 11, 9, 0, 0);
+      let epoch = Date.UTC(2020, 10, 11, 8, 0, 0);
       let nowUtc = new Date().getTime();
       let offsetMilliseconds = nowUtc - epoch;
       let offsetSeconds = Math.floor(offsetMilliseconds / 1000);
       let offsetMinutes = Math.floor(offsetSeconds / 60);
+      let offsetHours = Math.floor(offsetMinutes / 60);
       let currentRotation = Math.floor(offsetMinutes / 20) % this.rotation.length;
 
+      let currentMinute = Math.ceil((offsetMinutes % 60)/20)*20;
+      let currentHour = (offsetHours + 9) % 24;
       for (let i = 0; i < this.rotation.length; i++) {
+        let index = (i + currentRotation) % this.rotation.length;
+
+        let minute = currentMinute + i * 20;
+        let minuteToString = (minute % 60).toString().padStart(2, '0');
+
+        let hour = (currentHour + Math.floor(minute / 60)) % 24;
+        let hourToString = hour.toString().padStart(2, '0');
+
+        this.rotation[index].nextSpawn = `${hourToString}:${minuteToString}`;
+        this.rotation[i].sortOrder = index;
         this.rotation[i].active = false;
       }
       this.rotation[currentRotation].active = true;
