2d109aa5f2e0ab048fa16a13e8a707162b37b82d
Kristoffer Tverå <Bluekristoffer@gmail.com>
2020-11-15 21:45:57 +0100

    Added vue config file to set title / desc

---
+++ vue.config.js
@@ -0,0 +1,5 @@
+// vue.config.js
+module.exports = {
+    title: 'Prepatch Wizard - Amused to Death',
+    description: 'Navigate the upcomming spawn times of rares during the prepatch with this tool',
+  }
