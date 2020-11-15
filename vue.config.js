6f3fc3296d388502d2a308d958335d0bfe03c7fa
Kristoffer Tverå <Bluekristoffer@gmail.com>
2020-11-15 21:55:06 +0100

    Updated index to use title+description without webpack

--- vue.config.js
+++
@@ -1,5 +0,0 @@
-// vue.config.js
-module.exports = {
-    title: 'Prepatch Wizard - Amused to Death',
-    description: 'Navigate the upcomming spawn times of rares during the prepatch with this tool',
-  }
